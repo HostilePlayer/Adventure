67e998f904d7ecd34b67f10f5960deef946bbb97
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-27 00:09:34 +0200

    lavede et do/while loop i Interface

--- src/main/java/kea/dk/Interface.java
+++ src/main/java/kea/dk/Interface.java
@@ -20,43 +20,46 @@ public class Interface {
     public void brugerValg() {
         String brugerValg = sc.nextLine();
 
-        switch (brugerValg.trim().toLowerCase()) {
-            case "walk north":
-            case "go north":
-            case "north":
-            case "n":
-                System.out.println("going north");
-                break;
-            case "walk south":
-            case "go south":
-            case "south":
-            case "s":
-                System.out.println("going south");
-                break;
-            case "walk east":
-            case "go east":
-            case "east":
-            case "e":
-                System.out.println("going east");
-                break;
-            case "walk west":
-            case "go west":
-            case "west":
-            case "w":
-                System.out.println("going west");
-                break;
-            case "help":
-            case "h":
-                System.out.println("Here is a list of commands:\n" +
-                        "type (N)orth/(S)outh/(E)ast/(W)est to go that direction \n" +
-                        "type (look) to look around in your current location");
-                break;
-            case "look":
-            case "l":
-                break;
-            default:
-                System.out.println("if you need help type (help) to get help");
-                break;
-        }
+        do {
+            switch (brugerValg.trim().toLowerCase()) {
+                case "walk north":
+                case "go north":
+                case "north":
+                case "n":
+                    System.out.println("going north");
+                    break;
+                case "walk south":
+                case "go south":
+                case "south":
+                case "s":
+                    System.out.println("going south");
+                    break;
+                case "walk east":
+                case "go east":
+                case "east":
+                case "e":
+                    System.out.println("going east");
+                    break;
+                case "walk west":
+                case "go west":
+                case "west":
+                case "w":
+                    System.out.println("going west");
+                    break;
+                case "help":
+                case "h":
+                    System.out.println("Here is a list of commands:\n" +
+                            "type (N)orth/(S)outh/(E)ast/(W)est to go that direction \n" +
+                            "type (l)ook to look around in your current location \n" +
+                            "type (q)uit to exit program");
+                    break;
+                case "look":
+                case "l":
+                    break;
+                default:
+                    System.out.println("if you need help type (help) to get help");
+                    break;
+            }
+        } while (brugerValg == "quit" || brugerValg == "q");
     }
 }
