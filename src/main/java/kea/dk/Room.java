3195369e1a4dcd19515ac0f0431eb9f5b015e7a3
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-26 19:15:37 +0200

    startede på Room

--- src/main/java/kea/dk/Room.java
+++ src/main/java/kea/dk/Room.java
@@ -1,5 +1,23 @@
 package kea.dk;
 
 public class Room {
+    private String name;
+    private String description;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     //her skal vi lave vores room
 }
