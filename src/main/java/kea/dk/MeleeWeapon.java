452d3a4760dcb840f8ecb218d67065d49979b33d
AmingYee <wenm0007@stud.kea.dk>
2022-10-13 23:24:06 +0200

    remaking the classes fixed the error have to remake all the other things

--- src/main/java/kea/dk/MeleeWeapon.java
+++ src/main/java/kea/dk/MeleeWeapon.java
@@ -1,10 +1,8 @@
 package kea.dk;
 
-/*public class MeleeWeapon extends Weapons{
+public class MeleeWeapon extends Weapons{
 
-    public meleeWeapons(String name, int damage) {
+    public MeleeWeapon(String name, int damage) {
         super(name, damage);
     }
-    //merge conflict?
 }
-*/
--- src/main/java/kea/dk/RangedWeapon.java
+++ src/main/java/kea/dk/RangedWeapon.java
@@ -1,16 +1,14 @@
 package kea.dk;
 
-/*public class RangedWeapon extends Weapons{
+public class RangedWeapon extends Weapons{
     private int ammo;
 
-    public rangedWeapons(String name, int damage, int ammo) {
+    public RangedWeapon(String name, int damage, int ammo) {
         super(name, damage);
         this.ammo = ammo;
     }
+
     public int getAmmo(){
         return ammo;
     }
-    //merge conflict?
-
 }
-*/
